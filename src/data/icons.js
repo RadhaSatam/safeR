fbf54689f70e4c13d9716a6c79e446c2b5bdd94c
Radha Satam <radha.satam@gmail.com>
2018-09-08 19:33:15 -0700

    update readme

--- src/data/icons.js
+++ src/data/icons.js
@@ -1,18 +1,18 @@
 import L from 'leaflet';
 
 import hospital from '../imgs/hospital.png';
-import communityCenters from '../imgs/communityCenters.png';
+import community from '../imgs/community.png';
 import park from '../imgs/park.png';
 import seniorCenters from '../imgs/seniorCenters.png';
 import seniorHomes from '../imgs/seniorHomes.png';
 
 var LeafIcon = L.Icon.extend({
     options: {
-        iconSize: [20, 20],
+        iconSize: [24, 24],
     }
 });
 
-export const communityCentersIcon = new LeafIcon({iconUrl: communityCenters});
+export const communityCentersIcon = new LeafIcon({iconUrl: community});
 export const hospitalsIcon = new LeafIcon({iconUrl: hospital});
 export const parkPointsIcon = new LeafIcon({iconUrl: park});
 export const seniorCentersIcon =new LeafIcon({iconUrl: seniorCenters});
